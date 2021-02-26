85879e201a751d412b75aedca3960abeb713ba4f
mathenrifs <mateus_henrique2011@outlook.com>
2021-02-26 16:03:46 -0300

    creatProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,10 @@
 const Product = require('../models/productModel');
 
+const { getPostData } = require('../utils');
+
+// @desc Gets all Products
+// @route GET /api/products
+
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll();
@@ -11,6 +16,55 @@ async function getProducts(req, res) {
     }
 }
 
+// @desc Gets single Products
+// @route GET /api/products/:id
+
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if(!product) {
+            res.writeHead(404, {'Content-Type': 'aplication/json'});
+            res.end(JSON.stringify({ message: 'Product Not Found'}));
+        } else {
+            res.writeHead(200, {'Content-Type': 'aplication/json'});
+            res.end(JSON.stringify(product));
+        }
+
+        res.writeHead(200, {'Content-Type': 'aplication/json'});
+        res.end(JSON.stringify(products));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// @desc Create a Product
+// @route POST /api/products
+
+async function createProduct(req, res) {
+    try {
+        const body = await getPostData(req);
+
+        const { title, description, price} = JSON.parse(body);
+
+        const product = {
+            title, 
+            description, 
+            price
+        }
+
+        const newProduct = await Product.create(product);
+        
+        res.writeHead(201, {'Content-Type': 'aplication/json'});
+        return res.end(JSON.stringify(newProduct));
+
+    } catch (error) {
+        console.log(error);
+    } 
+    
+}
 module.exports = {
-    getProducts
+    getProducts, 
+    getProduct, 
+    createProduct
 }
